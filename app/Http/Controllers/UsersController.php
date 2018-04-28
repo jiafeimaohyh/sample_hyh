301b4231212ef2aebcb14903f70c67825c7c4243
jiafeimao <1157873987@qq.com>
2018-04-28 14:15:46 +0000

    Add account activations

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Auth;
+use Mail;
 class UsersController extends Controller
 {
 
     public function __construct()
     {
         $this->middleware('auth',[
-            'except'=>['create','show','store','index']
+            'except'=>['create','show','store','index','confirmEmail']
         ]);
 
         $this->middleware('guest', [
@@ -48,9 +49,35 @@ class UsersController extends Controller
         	'password'=>bcrypt($request->password)
 
         ]);
+        $this->sendEmailConfirmationTo($user);
+        session()->flash('success', '验证邮件已发送到你的注册邮箱上，请注意查收。');
+        return redirect('/');
+    }
+    protected function sendEmailConfirmationTo($user)
+    {
+        $view = 'emails.confirm';
+        $data = compact('user');
+        $from = 'aufree@yousails.com';
+        $name = 'Aufree';
+        $to = $user->email;
+        $subject = "感谢注册 Sample 应用！请确认你的邮箱。";
+
+        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
+            $message->from($from, $name)->to($to)->subject($subject);
+        });
+    }
+
+    public function confirmEmail($token)
+    {
+        $user = User::where(['activation_token'=>$token])->firstOrFail();
+        $user->activation_token = null;
+        $user->activated = true;
+        $user->save();
+
         Auth::login($user);
-        session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
-        return redirect()->route('users.show',[$user]);
+        session()->flash('success', '恭喜你，激活成功！');
+        return redirect()->route('users.show', [$user]);
+
     }
 
     //修改信息 页面
